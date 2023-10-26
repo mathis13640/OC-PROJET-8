53020f245e96d6a21d5cee282b9aee9a23c8c381
mathis13640 <73877129+mathis13640@users.noreply.github.com>
2023-10-26 18:11:34 +0200

    cards title click fixed

--- kasa/src/components/Card.jsx
+++ kasa/src/components/Card.jsx
@@ -3,22 +3,21 @@ import '../css/Home.css';
 import { NavLink } from "react-router-dom";
 import Logement from '../data/logements.json';
 
-
 function Card() {
   return (
     // Conteneur principal pour les cartes de logement
-      <div className='locations'>
-          {Logement.map((detail) => {
-              return (
-                  <div className="locate" key={detail.id}>
-                      <NavLink to={`/logement/${detail.id}`}>
-                        <img src={detail.cover} alt={detail.title}/>
-                        </NavLink>
+    <div className='locations'>
+        {Logement.map((detail) => {
+            return (
+                <div className="locate" key={detail.id}>
+                    <NavLink to={`/logement/${detail.id}`}>
+                      <img src={detail.cover} alt={detail.title}/>
                       <p>{detail.title}</p>
-                  </div>
-              )
-          })}
-      </div>
+                    </NavLink>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
